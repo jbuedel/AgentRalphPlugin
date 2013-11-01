7eeba8a4129460d695eced405b0e5d279cf3b42c
Josh Buedel <jbuedel@gmail.com>
2013-11-01 08:20:53 -0500

    Partial work on nice output for the CloneFinder tests.

--- Visualizer/Controllers/HomeController.cs
+++ Visualizer/Controllers/HomeController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI;
 using AgentRalph;
+using AgentRalph.CloneCandidateDetection;
+using ICSharpCode.NRefactory;
 using ICSharpCode.NRefactory.Ast;
+using ICSharpCode.NRefactory.Tests.Ast;
 using Microsoft.Ajax.Utilities;
+using Visualizer.Models;
 
 namespace Visualizer.Controllers
 {
@@ -45,6 +50,86 @@ namespace Visualizer.Controllers
           matchResult.FailNodeLeft.ToJson().id,
           matchResult.FailNodeRight.ToJson());
         return Json(r, JsonRequestBehavior.AllowGet);
+      }
+
+      public ActionResult CloneFinder()
+      {
+        var model = new CloneFinderModel();
+
+        string codeText = System.IO.File.ReadAllText(@"C:\Users\jbuedel\Projects\agentralphplugin\Ralph.Core.Tests\CloneCandidateDetectionTests\TestCases\CloneInDoWhileBlock.cs");
+        model.Code = codeText;
+
+        //            System.Diagnostics.Debugger.Break();
+
+        IParser parser = ParserFactory.CreateParser(SupportedLanguage.CSharp, new StringReader(codeText));
+        parser.Parse();
+
+        if (parser.Errors.Count > 0) {
+          model.Errors = parser.Errors.ErrorOutput;
+        }
+
+        var comments = parser.Lexer.SpecialTracker.RetrieveSpecials().OfType<Comment>();
+
+        if (comments.Any(x => x.CommentText.TrimStart().StartsWith("Ignore")))
+          throw new ApplicationException("Ignored.");
+
+        var begin_comment = comments.FirstOrDefault(x => x.CommentText.Contains("BEGIN"));
+        var end_comment = comments.FirstOrDefault(x => x.CommentText.Contains("END"));
+
+        if (begin_comment == null)
+          throw new ApplicationException("There was no comment containing the BEGIN keyword.");
+        if (end_comment == null)
+          throw new ApplicationException("There was no comment containing the END keyword.");
+
+        MethodsOnASingleClassCloneFinder cloneFinder = new MethodsOnASingleClassCloneFinder(new OscillatingExtractMethodExpansionFactory());
+
+        CloneDesc largest = null;
+
+        cloneFinder.OnExtractedCandidate += (finder, args) =>
+        {
+          if (largest == null)
+            largest = args.Candidate;
+          else if (args.Candidate.ReplacementInvocationInfo.ReplacementSectionStartLine >= begin_comment.StartPosition.Line
+               && args.Candidate.ReplacementInvocationInfo.ReplacementSectionEndLine <= end_comment.EndPosition.Line
+              && args.Candidate.PermutatedMethod.CountNodes() > largest.PermutatedMethod.CountNodes())
+            largest = args.Candidate;
+
+        };
+
+
+        var replacements = cloneFinder.GetCloneReplacements(parser.CompilationUnit);
+
+        int clone_count = replacements.Clones.Count;
+
+        model.CloneCount = clone_count;
+        for (int i = 0; i < replacements.Clones.Count; i++) {
+          //TestLog.EmbedPlainText("   ***** Clone #" + i + " *****", replacements.Clones[i].ToString());
+        }
+
+        model.Largest = largest;
+
+        var quickFixInfos = replacements.Clones.Where(Predicate(begin_comment, end_comment));
+
+	if(quickFixInfos.Count() > 0)
+          ModelState.AddModelError("", "None of the clones found (there were " + clone_count + ") fell inbetween the BEGIN/END markers.");
+
+        var expected_call_snippet = begin_comment.CommentText.Substring(begin_comment.CommentText.IndexOf("BEGIN") + 5).Trim();
+        if (!string.IsNullOrEmpty(expected_call_snippet)) {
+          var expected_call = ParseUtilCSharp.ParseStatement<Statement>(expected_call_snippet);
+          var actual_cal = ParseUtilCSharp.ParseStatement<Statement>(quickFixInfos.First().TextForACallToJanga);
+          if (expected_call.MatchesPrint(actual_cal)) {
+            ModelState.AddModelError("", "The expected call did not match the actual call.  \n\tExpected Call: " + expected_call.Print() + "\n\t" + "Actual Call: " + actual_cal.Print());
+          }
+        }
+
+        return View(model);
+      }
+
+      private Func<QuickFixInfo, bool> Predicate(Comment begin_comment, Comment end_comment)
+      {
+        return x =>
+               x.ReplacementSectionStartLine > begin_comment.StartPosition.Line &&
+               x.ReplacementSectionEndLine < end_comment.EndPosition.Line;
       }
     }
 }
---
+++ Visualizer/Models/CloneFinderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AgentRalph.CloneCandidateDetection;
+
+namespace Visualizer.Models
+{
+  public class CloneFinderModel
+  {
+    public string Code { get; set; }
+    public string Errors { get; set; }
+    public int CloneCount { get; set; }
+    public CloneDesc Largest { get; set; }
+  }
+}
