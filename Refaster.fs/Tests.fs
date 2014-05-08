e1fb3d943708ca2860a8e04c725451387fd7ee5d
Josh Buedel <jbuedel@gmail.com>
2014-05-08 08:01:24 -0500

    Refactored asserts into individual tests.

--- Refaster.fs/Tests.fs
+++ Refaster.fs/Tests.fs
@@ -1,4 +1,4 @@
-﻿module Refaster.fs
+﻿module Refaster.Tests
 open NUnit.Framework
 open FsUnit
 open ICSharpCode.NRefactory.Ast
@@ -21,7 +21,7 @@ let applyPattern (pat:Pattern) exp : Match option =
   None
 
 [<TestFixture>]
-type Class1() = 
+type RefasterTests() = 
   let isSome fopt md =
     match fopt md with
     | Some(p) -> Assert.Pass()
@@ -39,43 +39,45 @@ type Class1() =
 
   let print (expr:Expression) = 
     ICSharpCode.NRefactory.INodeExt.Print(expr)
-   
+
+  let doMatch patText exprText =
+    toMethod patText |> toPattern |> getPattern |> applyPattern <| toExpr exprText
+
+  let testF patText exprText = 
+    let mtch = doMatch patText exprText 
+    match mtch with
+    | Some(m) -> Assert.Fail("Got a match")
+    | None    -> Assert.Pass()
+
+  let test patText exprText = 
+    let mtch = doMatch patText exprText 
+    match mtch with
+    | Some(m) -> m
+    | None    -> failwith "Expected a match"
+  
   [<Test>]
-  member this.MethodDeclarationsBecomePatterns() =
+  member this.``MethodDeclarations become patterns``() =
     let md = toMethod "void foo(){Console.WriteLine(13);}"
     isSome toPattern md
 
   [<Test>]
-  member this.``Patterns match against expressions``() =
-    let doMatch patText exprText note =
-      toMethod patText |> toPattern |> getPattern |> applyPattern <| toExpr exprText
-
-    let testF patText exprText note = 
-      let mtch = doMatch patText exprText note
-      match mtch with
-      | Some(m) -> Assert.Fail("Got a match")
-      | None    -> Assert.Pass()
-
-    let test patText exprText note = 
-      let mtch = doMatch patText exprText note
-      match mtch with
-      | Some(m) -> m
-      | None    -> failwith "Expected a match"
-
-
-
-    let result = test "void pat(){Console.WriteLine(13);}" "Console.WriteLine(13)" "Simplest case"
+  member this.``simplest case``() =
+    let result = test "void pat(){Console.WriteLine(13);}" "Console.WriteLine(13)" 
     match result with
     | Match([]) -> () 
     | _ -> Assert.Fail("Expected a match")
 
-    let result = test "void pat(int x){Console.WriteLine(x);}" "Console.WriteLine(13)" "parameters can match expressions"
+  [<Test>]
+  member this.``parameters can match expressions``() =
+    let result = test "void pat(int x){Console.WriteLine(x);}" "Console.WriteLine(13)" 
     match result with
     | Match((name,exp) :: []) -> name |> should equal "x"
                                  exp |> print |> should equal "13" 
     | _ -> Assert.Fail("Expected a match")
 
-    let result = test "void pat(int x, string y){Console.WriteLine(x,y);}" "Console.WriteLine(13, \"string\")" "multiple parameters match multiple expressions"
+  [<Test>]
+  member this.``multiple parameters match multiple expressions``() =
+    let result = test "void pat(int x, string y){Console.WriteLine(x,y);}" "Console.WriteLine(13, \"string\")" 
     match result with
     | Match((name1,expr1) :: (name2,expr2) :: []) -> name1 |> should equal "x"
                                                      expr1 |> print |> should equal "13" 
@@ -83,19 +85,27 @@ type Class1() =
                                                      expr2 |> print |> should equal "\"string\"" 
     | _ -> Assert.Fail("Expected a match")
 
-    testF "void pat(int x){Console.WriteLine(x);}" "Console.WriteLine(\"string\")" "parameter types must be compatible with the type of expression they match"
+  [<Test>]
+  member this.``parameter types must be compatible with the type of expression they match``() =
+    testF "void pat(int x){Console.WriteLine(x);}" "Console.WriteLine(\"string\")" 
 
-    let result = test "void pat(int x, string y){Console.WriteLine(x,y);}" "Console.WriteLine(13)" "parameters that do not match are not included in the result"
+  [<Test>]
+  member this.``parameters that do not match are not included in the result``() =
+    let result = test "void pat(int x, string y){Console.WriteLine(x,y);}" "Console.WriteLine(13)" 
     match result with
     | Match((name,exp) :: []) -> name |> should equal "x"
                                  exp |> print |> should equal "13" 
     | _ -> Assert.Fail("Expected a match")
 
-    let result = test "void pat(int x){Console.WriteLine(x, x);}" "Console.WriteLine(13, 13)" "a single parameter can match multiple expressions"
+  [<Test>]
+  member this.``a single parameter can match multiple expressions``() =
+    let result = test "void pat(int x){Console.WriteLine(x, x);}" "Console.WriteLine(13, 13)" 
     match result with
     | Match((name,expr) :: []) -> name |> should equal "x"
                                   expr |> print |> should equal "13"
     | _ -> Assert.Fail("Expected a match")
 
-    testF "void pat(int x){Console.WriteLine(x, x);}" "Console.WriteLine(13, 14)" "a single parameter can match multiple expressions, but the expressions must be identical"
+  [<Test>]
+  member this.``a single parameter can match multiple expressions, but the expressions must be identical``() =
+    testF "void pat(int x){Console.WriteLine(x, x);}" "Console.WriteLine(13, 14)" 
 
