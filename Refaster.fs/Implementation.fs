8288c22ebd6f04ecca5e334823a5b407274415fb
Josh Buedel <jbuedel@gmail.com>
2014-05-09 07:46:35 -0500

    Pattern parameter matches an expression.

--- Refaster.fs/Implementation.fs
+++ Refaster.fs/Implementation.fs
@@ -1,26 +1,34 @@
 ﻿module Refaster
+open System.Collections.Generic
 open ICSharpCode.NRefactory.Ast
 
-type Pattern(expr) =
-  member this.Expr = expr
-  member this.Params = []
+type Pattern(Expr, Params) =
+  member this.Expr = Expr
+  member this.Params = Params
 
 type Match =
-| Match of (string*Expression) list
+| Match of (string*INode) list
 
 type PatternMatchVisitor(parms : string list) = 
   inherit AgentRalph.Visitors.AstComparisonVisitor() 
-  override this.VisitIdentifierExpression(pat, obj) = printf "%A" pat
-                                                      true
+  let mutable stuff : (string*INode) list = []
+  member public this.Locations = stuff
+  override this.VisitIdentifierExpression(pat, obj) = printfn "%A" pat
+                                                      match List.tryFind (fun p -> p = pat.Identifier) parms with
+                                                      | Some(p) -> stuff <- List.append [(p, obj:?>INode)] stuff
+                                                                   true
+                                                      | None -> base.VisitIdentifierExpression(pat,obj)
 
 let toPattern (md:MethodDeclaration) : Pattern option =
   let expr = md.Body.Children.[0]
   let expr = (expr :?> ExpressionStatement).Expression
-  Some(Pattern(expr))
+  let p's = md.Parameters |> Seq.toList|> List.map (fun p -> p.ParameterName)
+  Some(Pattern(expr, p's))
   
 let applyPattern (pat:Pattern) exp : Match option =
   let visitor = new PatternMatchVisitor(pat.Params)
   let success = pat.Expr.AcceptVisitor(visitor, exp)
   if success then
-    Some(Match([]))
+    let locations = visitor.Locations 
+    Some(Match(locations))
   else None
