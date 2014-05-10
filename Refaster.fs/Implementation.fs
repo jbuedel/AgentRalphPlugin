4bf6c55da0a66c056d7ed43970f59a04e50461db
Josh Buedel <jbuedel@gmail.com>
2014-05-09 20:45:22 -0500

    Collect type information for capture groups (even though we don't use it yet)

--- Refaster.fs/Implementation.fs
+++ Refaster.fs/Implementation.fs
@@ -9,24 +9,24 @@ type Pattern(Expr, Params) =
 type Match =
 | Match of (string*INode) list
 
-type PatternMatchVisitor(parms : string list) = 
+type PatternMatchVisitor(parms : (string*string) list) = 
   inherit AgentRalph.Visitors.AstComparisonVisitor() 
   let mutable stuff : (string*INode) list = []
   let isIdentical cap (node:INode) = AgentRalph.AstMatchHelper.Matches(cap,node)
   member public this.CaptureGroups = stuff |> List.rev // the rev allows tests to depend on order.
   override this.VisitIdentifierExpression(pat, obj) = let obj = obj :?> INode
-                                                      match List.tryFind (fun p -> p = pat.Identifier) parms with
-                                                      | Some(p) -> match stuff |> List.tryFind (fun (name,cap) -> name = pat.Identifier) with
-                                                                   | Some(name,cap) -> isIdentical cap obj
-                                                                   | None           -> stuff <- List.append [(p, obj)] stuff 
-                                                                                       true
+                                                      match List.tryFind (fun (pname,_) -> pname = pat.Identifier) parms with
+                                                      | Some((pname,_)) -> match stuff |> List.tryFind (fun (name,cap) -> name = pat.Identifier) with
+                                                                           | Some(name,cap) -> isIdentical cap obj
+                                                                           | None           -> stuff <- List.append [(pname, obj)] stuff 
+                                                                                               true
                                                       | None -> base.VisitIdentifierExpression(pat,obj)
 
 let toPattern (md:MethodDeclaration) : Pattern option =
   let expr = md.Body.Children.[0]
   let expr = (expr :?> ExpressionStatement).Expression
-  let p's = md.Parameters |> Seq.toList|> List.map (fun p -> p.ParameterName)
-  Some(Pattern(expr, p's))
+  let capgrps = md.Parameters |> Seq.toList|> List.map (fun p -> p.ParameterName, p.TypeReference.ToString()) // ToString() does a decent job of getting a full type name
+  Some(Pattern(expr, capgrps))
   
 let applyPattern (pat:Pattern) exp : Match option =
   let visitor = new PatternMatchVisitor(pat.Params)
